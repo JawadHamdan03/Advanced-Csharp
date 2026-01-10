29a7688c0ff1ff9fbfceefb8d9b99ba1a0aacf9e
JawadHamdan03 <eng.jawad121@hotmail.com>
2026-01-10 19:52:39 +0200

    Race Conditions and lock

--- Program.cs
+++ Program.cs
@@ -7,78 +7,80 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        // Race Condition
+        // both the threads t1 and t2 race to execute Debit() 
+        // we use lock to prevent race condition
 
+        var wallet = new Wallet("jawad",50);
 
-        // the default for a thread is to be foreground 
-        Thread.CurrentThread.Name = "Main Thread";
-        System.Console.WriteLine(Thread.CurrentThread.Name);
-        Console.WriteLine($"BackGround Thread : {Thread.CurrentThread.IsBackground}");
-
-        var wallet = new Wallet("Jawad",80);
-
-        Thread t1 = new Thread(wallet.RunRandomTransactions);
-        t1.Name = "T1";
-
-        Console.WriteLine($"T1 after decleration : {t1.ThreadState}");
+        Thread t1 = new Thread(()=>wallet.Debit(40));
+        Thread t2 = new Thread(()=>wallet.Debit(40));
 
         t1.Start();
-        Console.WriteLine($"T1 after start : {t1.ThreadState}");
-
-        t1.Join(); // after t1 finishes, t2 starts 
-
-        var t2 = new Thread(new ThreadStart(wallet.RunRandomTransactions));
-        t2.Name = "T2";
         t2.Start();
 
+
+        t1.Join();
+        t2.Join();
+
+        Console.WriteLine(wallet);
         Console.ReadKey();
     }
 }
 
-    class Wallet
+class Wallet
+{
+    private readonly object _bitcoinsLock= new object();
+
+
+    public string Name { get; private set; }
+    public int Bitcoin { get; private set; }
+
+    public Wallet(string name, int Bitcoin)
     {
-        public string Name { get; private set; }
-        public int Bitcoin { get; private set; }
+        this.Name = name;
+        this.Bitcoin = Bitcoin;
+    }
 
-        public Wallet(string name, int Bitcoin)
-        {
-            this.Name = name;
-            this.Bitcoin = Bitcoin;
-        }
+    public void Debit(int amount)
+    {
 
-        public void Debit(int amount)
+        lock (_bitcoinsLock)
         {
+         if (Bitcoin > amount)
+           {
             Thread.Sleep(1000);
             Bitcoin -= amount;
-            System.Console.WriteLine($"Thread Id :{Thread.CurrentThread.ManagedThreadId} ," +
-                $"Thread Name : {Thread.CurrentThread.Name} ,Processor Id :{Thread.GetCurrentProcessorId()}");
+           }
         }
-        public void Credit(int amount)
-        {
-            Thread.Sleep(1000);
-            Bitcoin += amount;
-            System.Console.WriteLine($"Thread Id :{Thread.CurrentThread.ManagedThreadId} ," +
-                $"Thread Name : {Thread.CurrentThread.Name} , Processor Id :{Thread.GetCurrentProcessorId()}");
-        }
-
-        public void RunRandomTransactions()
-        {
-            int[] amounts = { 10, 20, 30, -20, 10, -10, 30, -10, 40, -20 };
-
-            foreach (var amount in amounts)
-            {
-                var absValue = Math.Abs(amount);
-                if (amount < 0)
-                    Debit(absValue);
-
-                else
-                    Credit(absValue);
-            }
-        }
-
-        public override string ToString()
-        {
-            return $"Name :{Name} , Bitcoin : {Bitcoin}";
-        }
-
+    
+   
     }
+    public void Credit(int amount)
+    {
+        Thread.Sleep(1000);
+        Bitcoin += amount;
+    }
+
+    public void RunRandomTransactions()
+    {
+        int[] amounts = { 10, 20, 30, -20, 10, -10, 30, -10, 40, -20 };
+
+        foreach (var amount in amounts)
+        {
+            var absValue = Math.Abs(amount);
+            if (amount < 0)
+                Debit(absValue);
+
+            else
+                Credit(absValue);
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"Name :{Name} , Bitcoin : {Bitcoin}";
+    }
+
+}
 
--- obj/Debug/net10.0/AdvancedCsharp.AssemblyInfo.cs
+++ obj/Debug/net10.0/AdvancedCsharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdvancedCsharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+236b040bb2dc480ccbae70128d20c9df571ddfe1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a29205ab06143622115588b9997caeef409d09b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdvancedCsharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdvancedCsharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
